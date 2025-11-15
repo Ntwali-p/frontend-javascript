2be91c52c51c542b96b7c2ff5866d6aad5c0cd8b
Ntwali-p <ntwali.pacific@gmail.com>
2025-11-15 06:36:25 +0200

    changes

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,10 @@
+// task_1/js/main.ts
+// Example usage
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
+console.log(director1);
