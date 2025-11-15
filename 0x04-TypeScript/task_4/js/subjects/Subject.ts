6d72f8288568e951bb488178c4cc89f7d89c603c
Ntwali-p <ntwali.pacific@gmail.com>
2025-11-15 23:36:29 +0200

    changes

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,13 @@
 namespace Subjects {
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+
   export class Subject {
     teacher: Teacher;
 
+    // This was missing
     setTeacher(teacher: Teacher) {
       this.teacher = teacher;
     }
