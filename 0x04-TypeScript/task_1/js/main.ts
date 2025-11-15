ffd5d677aed748db0c2d363fbc5e443dceb166d2
Ntwali-p <ntwali.pacific@gmail.com>
2025-11-15 07:21:13 +0200

    changes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -8,7 +8,12 @@ interface Teacher {
   [key: string]: any; // Allow any additional attributes
 }
 
-// Example usage
+// Directors interface that extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage for Teacher
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -18,12 +23,8 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
-// Directors interface extending Teacher
-interface Directors extends Teacher {
-  numberOfReports: number;
-}
 
-// Example usage
+// Example usage for Directors
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -31,4 +32,50 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
+
 console.log(director1);
+
+// PrintTeacher function interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// PrintTeacher function implementation
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Interface for StudentClass constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Interface for StudentClass methods
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// StudentClass implementation
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student = new StudentClass('John', 'Doe');
+console.log(student.displayName());
+console.log(student.workOnHomework());
